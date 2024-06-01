e662bd8afd9b2ac51a334c131c2a5bf33a4276cf
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2024-06-01 18:23:14 -0300

    feat: add NewTransactionModal Dialog

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,8 @@
 import { ChartLine } from "phosphor-react";
+import * as Dialog from "@radix-ui/react-dialog";
 
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import { NewTransactionModal } from "../NewtransactionModal";
 
 export function Header() {
   return (
@@ -8,7 +10,13 @@ export function Header() {
       <HeaderContent>
         <ChartLine size={32} />
 
-        <NewTransactionButton>Nova Transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova Transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
