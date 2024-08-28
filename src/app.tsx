3b30e9a91ad518c41042118239ad6e8207e26aa3
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2024-08-28 19:35:29 -0300

    chore: react-router-dom setup

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,6 @@
 import { ThemeProvider } from "styled-components";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
@@ -9,15 +10,24 @@ import { TransactionProvider } from "./contexts/TransactionsContext";
 
 const queryClient = new QueryClient();
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <TransactionProvider>
+        <Transactions />
+      </TransactionProvider>
+    ),
+  },
+]);
+
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
 
       <QueryClientProvider client={queryClient}>
-        <TransactionProvider>
-          <Transactions />
-        </TransactionProvider>
+        <RouterProvider router={router} />
       </QueryClientProvider>
     </ThemeProvider>
   );
